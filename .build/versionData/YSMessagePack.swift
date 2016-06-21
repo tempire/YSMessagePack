1eb5f818867e17a25f48960b3778e10a3a15316d
Yuji <hatsuneyuji@yahoo.co.jp>
2016-06-20 22:18:14 -0700

    add missing files

---
+++ .build/versionData/YSMessagePack.swift
@@ -0,0 +1,5 @@
+public let url: String = "/Users/yuuji/Documents/YSMessagePack"
+public let version: (major: Int, minor: Int, patch: Int, prereleaseIdentifiers: [String], buildMetadata: String?) = (1, 6, 0, [], nil)
+public let versionString: String = "1.6.0"
+public let sha: String? = "Optional("43bb0da8c493b7287d7c4d83e8a5fa58e67e4c9e")"
+public let modified: Bool = true
